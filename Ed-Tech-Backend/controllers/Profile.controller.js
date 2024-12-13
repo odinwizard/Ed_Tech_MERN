1efe30baed49d83f138f7d238301c50d3ad01dd9
Anindya Manna <anindyamanna2013@gmail.com>
2024-12-13 21:41:37 +0530

    Add profile handler.

--- Ed-Tech-Backend/controllers/Profile.controller.js
+++ Ed-Tech-Backend/controllers/Profile.controller.js
@@ -20,9 +20,74 @@ exports.updateProfile = async (req, res) => {
         const profileId = userDetails.additionalDetails;
         const profileDetails = await Profile.findById(profileId);
         //update profile
-        
+        profileDetails.dateOfBirth = dateOfBirth;
+        profileDetails.about = about;
+        profileDetails.gender = gender;
+        profileDetails.contactNumber = contactNumber;
+        await profileDetails.save();
         //return response
+        return res.status(200).json({
+            success:true,
+            message:"Profile updated successfully",
+            profileDetails,
+        });
     } catch (error) {
-        
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
     }
 };
+
+exports.deleteAccount = async (req, res) => {
+    try {
+        //get id
+        const id = req.user.id;
+        //validation
+        const userDetails = await User.findById(id);
+        if(!userDetails) {
+            return res.status(404).json({
+                success:false,
+                message:"User not found",
+            });
+        }
+        //delet profile
+        await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
+        //delet user
+        await User.findByIdAndDelete({_id:id});
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"User deleted successfully",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"User cannot deleted",
+        })
+    }
+}
+
+exports.getAllUserDetails = async (req, res) => {
+    try {
+        const id = req.user.id;
+
+        const userDetails = await User.findById(id).populate("additionalDetails").exec();
+
+        return res.status(200).json({
+            success:true,
+            message:"User data fetched successfully",
+            userDetails,
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+};
+
+
