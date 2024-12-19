09af98226dab2afbcf613d097759b33c0977d0d4
Anindya Manna <anindyamanna2013@gmail.com>
2024-12-19 23:25:50 +0530

    Add rating and reviews handler. changes tag to category.

---
+++ Ed-Tech-Backend/controllers/Category.controllers.js
@@ -0,0 +1,88 @@
+const Category = require("../models/Category.models");
+
+//create Category handler function..
+exports.createCategory = async (req, res) => {
+    try {
+        //fetch data
+        const {name, description} = req.body;
+        //validation
+        if(!name || !description) {
+            return res.status(400).json({
+                success: false,
+                message:"All fields are required",
+            })
+        }
+        //create entry in DB
+        const categoryDetails = await Category.create({
+            name:name,
+            description:description,
+        });
+        console.log(categoryDetails);
+        //return response
+        return res.status(200).json({
+            success: true,
+            message:"Category created successfully",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+};
+
+//getAllCategorys........
+
+exports.showAllCategories = async (req, res) => {
+    try {
+        const allCategorys = await Category.find({}, {name:true, description:true});
+        return res.status(200).json({
+            success: true,
+            message:"All Categorys return successfully",
+            allCategorys,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
+
+//CategoryPage details...
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        //get category Id
+        const {categoryId} = req.body;
+        //get couses for specified category id
+        const selectedCategory = await Category.findById(categoryId)
+                                                .populate("course")
+                                                .exec();
+        //validation
+        if(!selectedCategory) {
+            return res.status(404).json({
+                success:false,
+                message:"Data not found",
+            });
+        }
+        //get courses for different categories
+        const differentCategories = await Category.find({
+                                            _id: {$ne: categoryId},
+        }).populate("course").exec();
+        //get top selling courses
+        //todo
+        //return response
+        return res.status(200).json({
+            success:true,
+            data: {
+                selectedCategory,
+                differentCategories,
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
--- Ed-Tech-Backend/controllers/RatingAndReviews.controllers.js
+++ Ed-Tech-Backend/controllers/RatingAndReviews.controllers.js
@@ -1,5 +1,6 @@
 const RatingAndReview = require("../models/RatingAndReview.models");
 const Course = require("../models/Course.models");
+const { default: mongoose } = require("mongoose");
 
 //createRating
 exports.createRating = async (req, res) => {
@@ -64,7 +65,35 @@ exports.createRating = async (req, res) => {
 //getAverageRating
 exports.getAvarageRating = async (req, res) => {
     try {
-        //....tobe continue..
+        //get course Id
+        const courseId = req.body.courseId;
+        //calculate avg rating
+        const result = await RatingAndReview.aggregate([
+            {
+                $match:{
+                    course: new mongoose.Types.ObjectId(courseId),
+                },
+            },
+            {
+                $group: {
+                    _id: null,
+                    averageRating: {$avg: "$rating"},
+                }
+            }
+        ])
+        //return rating
+        if(result.length > 0) {
+            return res.status(200).json({
+                success:true,
+                averageRating: result[0].averageRating,
+            })
+        }
+        //if rating and review is not exist..
+        return res.status(200).json({
+            success:true,
+            message:"No rating given till now",
+            averageRating:0,
+        })
     } catch (error) {
         console.error(error);
         return res.status(500).json({
@@ -75,3 +104,30 @@ exports.getAvarageRating = async (req, res) => {
 }
 
 //getAllRating
+
+exports.getAllRatingAndReviews = async (req, res) => {
+    try {
+            const allReviews = await RatingAndReview.find({})
+                                                    .sort({rating: "desc"})
+                                                    .populate({
+                                                        path:"user",
+                                                        select:"firstName lastName email image",
+                                                    })
+                                                    .populate({
+                                                        path:"course",
+                                                        select:"courseName",
+                                                    })
+                                                    .exec();
+            return res.status(200).json({
+                success:true,
+                message:"All reviews fetched successfully",
+                data:allReviews,
+            });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
