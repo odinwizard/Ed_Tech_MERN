c10c14e0af69d86955c621e7d944e7d1e896edc0
Anindya Manna <anindyamanna2013@gmail.com>
2025-01-06 23:28:09 +0530

    Add navbar

--- Ed-Tech-Frontend/src/App.js
+++ Ed-Tech-Frontend/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Navbar } from "./components/common/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
    <div className="w-screen min-h-screen bg-richblue-900 flex flex-col font-inter">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
