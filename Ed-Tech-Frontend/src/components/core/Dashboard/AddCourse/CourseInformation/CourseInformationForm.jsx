8a94c19907961d97e0794b4b660ddde402fa4b15
Anindya Manna <anindyamanna2013@gmail.com>
2025-02-28 23:48:47 +0530

    Add Enhance CourseInformationForm and RequirementField: implement form update checks, add requirement management, and improve submission handling

--- Ed-Tech-Frontend/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ Ed-Tech-Frontend/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { HiOutlineCurrencyRupee } from "react-icons/hi";
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchCourseCategories } from '../../../../../services/operations/courseDetailsAPI';
+import { setCourse, setStep } from '../../../../../../src/slices/courseSlice';
+import { addCourseDetails, editCourseDetails, fetchCourseCategories } from '../../../../../services/operations/courseDetailsAPI';
+import { COURSE_STATUS } from '../../../../../utils/constants';
+import { IconBtn } from '../../../../common/IconBtn';
 import { RequirementField } from './RequirementField';
+
 
 export const CourseInformationForm = () => {
 
@@ -16,6 +21,7 @@ export const CourseInformationForm = () => {
     } = useForm();
 
     const dispatch = useDispatch();
+    const {token} = useSelector((state)=>state.auth);
     const {course, editCourse} = useSelector((state) => state.course);
     const [loading, setLoading] = useState(false);
     const [courseCategories, setCourseCategories] = useState([]);
@@ -45,9 +51,95 @@ export const CourseInformationForm = () => {
         
     },[])
 
-    const onSubmit = async (data) => {
-        
+    const isFormUpdated = () => {
+        const currentValues = getValues();
+        if(currentValues.courseTitle !== course.courseName ||
+            currentValues.courseShortDesc !== course.courseDescription ||
+            currentValues.coursePrice !== course.price ||
+            currentValues.courseTitle !== course.courseName ||
+            //currentValues.courseTags.toString() !== course.tag.toString() ||
+            currentValues.courseBenefits !== course.whatYouWillLearn ||
+            currentValues.courseCategory._id !== course.category._id ||
+            //currentValues.courseImage !== course.thumbnail ||
+            currentValues.courseRequirements.toString() !== course.instructions.toString() )
+            return true;
+        else
+            return false;
     }
+
+    const onSubmit = async(data) => {
+
+        if(editCourse) {
+            if(isFormUpdated()) {
+                const currentValues = getValues();
+            const formData = new FormData();
+
+            formData.append("courseId", course._id);
+            if(currentValues.courseTitle !== course.courseName) {
+                formData.append("courseName", data.courseTitle);
+            }
+
+            if(currentValues.courseShortDesc !== course.courseDescription) {
+                formData.append("courseDescription", data.courseShortDesc);
+            }
+
+            if(currentValues.coursePrice !== course.price) {
+                formData.append("price", data.coursePrice);
+            }
+
+            if(currentValues.courseBenefits !== course.whatYouWillLearn) {
+                formData.append("whatYouWillLearn", data.courseBenefits);
+            }
+
+            if(currentValues.courseCategory._id !== course.category._id) {
+                formData.append("category", data.courseCategory);
+            }
+
+            if(currentValues.courseRequirements.toString() !== course.instructions.toString()) {
+                formData.append("instructions", JSON.stringify(data.courseRequirements));
+            }
+
+            setLoading(true);
+            const result = await editCourseDetails(formData, token);
+            setLoading(false);
+            if(result) {
+                setStep(2);
+                dispatch(setCourse(result));
+            }
+            } 
+            else {
+                toast.error("NO Changes made so far");
+            }
+            // console.log("PRINTING FORMDATA", formData);
+            // console.log("PRINTING result", result);
+
+            return;
+        }
+
+        //create a new course
+        const formData = new FormData();
+        formData.append("courseName", data.courseTitle);
+        formData.append("courseDescription", data.courseShortDesc);
+        formData.append("price", data.coursePrice);
+        formData.append("whatYouWillLearn", data.courseBenefits);
+        formData.append("category", data.courseCategory);
+        formData.append("instructions", JSON.stringify(data.courseRequirements));
+        formData.append("status", COURSE_STATUS.DRAFT);
+
+        setLoading(true);
+        console.log("BEFORE add course API call");
+        console.log("PRINTING FORMDATA", formData);
+        const result = await addCourseDetails(formData,token);
+        if(result) {
+            setStep(2);
+            dispatch(setCourse(result));
+        }
+        setLoading(false);
+        console.log("PRINTING FORMDATA", formData);
+        console.log("PRINTING result", result);
+
+    }
+
 
   return (
    <form
@@ -166,6 +258,21 @@ export const CourseInformationForm = () => {
             setValue={setValue}
             getValues={getValues}
         />
+        <div>
+            {
+                editCourse && (
+                    <button
+                    onClick={() => dispatch(setStep(2))}
+                    className='flex items-center gap-x-2 bg-richblack-300'
+                    >
+                        Continue without Saving
+                    </button>
+                )
+            }
+            <IconBtn
+                text={!editCourse ? "Next" : "Save Changes"}
+            />
+        </div>
 
    </form>
   )
--- Ed-Tech-Frontend/src/components/core/Dashboard/AddCourse/CourseInformation/RequirementField.jsx
+++ Ed-Tech-Frontend/src/components/core/Dashboard/AddCourse/CourseInformation/RequirementField.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const RequirementField = ({name, label, register, errors, setValue, getValues}) => {
 
     const [requirement, setRequirement] = useState("");
     const [requirementList, setRequirementList] = useState([]);
+
+
+    useEffect(() => {
+        register(name, {
+            required:true,
+            validate: (value) => value.length > 0
+        })
+    },[])
+
+    useEffect(() => {
+        setValue(name, requirementList
+        );
+    }, [requirementList]);
 
     const handleAddRequirement = () => {
         if(requirement) {
@@ -20,13 +33,50 @@ export const RequirementField = ({name, label, register, errors, setValue, getVa
   return (
     <div>
         
-        <label>{label}<sup>*</sup></label>
+        <label htmlFor={name}>{label}<sup>*</sup></label>
             <div>
                 <input
                     type='text'
-                    id={requirement}
+                    id={name}
+                    value={requirement}
+                    onChange={(e) => setRequirement(e.target.value)}
+                    className='w-full'
                 />
+                <button
+                    type='button'
+                    onClick={handleAddRequirement}
+                    className='font-semibold text-yellow-50'
+                >
+                    Add
+                </button>
             </div>
+            {
+                requirementList.length > 0 && (
+                    <ul>
+                        {
+                            requirementList.map( (requirement, index) => (
+                                <li key={index} className='flex items-center text-richblack-5'>
+                                    <span>{requirement}</span>
+                                    <button
+                                    type='button'
+                                    onClick={ () => handleRemoveRequirement(index)}
+                                    className='text-xs text-pure-greys-300'
+                                    >
+                                        clear
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                )
+            }
+            {
+                errors[name] && (
+                    <span>
+                      {label} is required
+                    </span>
+                )
+            }
     </div>
   )
 }
